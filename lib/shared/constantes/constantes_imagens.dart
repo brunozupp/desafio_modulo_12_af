5baa3b010f99c429fd9a61f393da09487077931e
bruno.noveli <brunimzuppwolf@gmail.com>
2022-05-09 14:04:10 -0300

    Trocado foto de perfil

--- lib/shared/constantes/constantes_imagens.dart
+++ lib/shared/constantes/constantes_imagens.dart
@@ -4,7 +4,7 @@ class ConstantesImagens {
 
   static const instagramLogo = "assets/imagens/instagram_logo.png";
 
-  static const perfil = "https://scontent-gru2-2.xx.fbcdn.net/v/t1.6435-9/201289668_2050074035154810_5316967695378847421_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHb7KvcRpLpIqjk9phxxaY4nZiM4e_0_1idmIzh7_T_WLlSd9ttADt7GWXmMa8cz5i7uNXFC4jQwubfx_309aKV&_nc_ohc=fuRH4Mm253wAX95UDVl&_nc_ht=scontent-gru2-2.xx&oh=00_AT-ISG-gmm2YAMLCJentC58HXdgspVy9Y7GmSVhHA2E2IQ&oe=625C4974";
+  static const perfil = "https://sm.ign.com/ign_br/screenshot/default/naruto-shippuden_f134.png";
 
   static const feed1 = "https://observatoriodocinema.uol.com.br/wp-content/uploads/2021/10/pokemon-movie.jpg";
 }
